5997ca2659a56bf48bb5ae0ef350cfecfb4ff84f
joaonery1 <joao.nery1@hotmail.com>
2023-09-01 09:57:57 -0300

    exercise 1.7

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,21 @@
 # bounce.py
+# A rubber ball is dropped from a height of 100 meters
+# bounce 3/5 height
 #
+
+
 # Exercise 1.5
+
+
+height = 100 #meters
+quicks = 3/5
+height_ball = height * quicks
+bounces = 10
+i = 0
+#print(height_ball)
+while i <10 :
+    height_ball = round(height_ball,4)
+    print(height_ball)
+    height_ball = height_ball * quicks
+    i = i +1
+
